7d081ea753ea29e0faf5a51ff4cf1c53d0753a0d
Doug Reed <dreed5@wisc.edu>
2017-07-31 14:55:52 -0500

    More development

--- my-app-seed-war/src/main/webapp/my-app/view-home/controllers.js
+++ my-app-seed-war/src/main/webapp/my-app/view-home/controllers.js
@@ -7,6 +7,43 @@ define(['angular', 'jquery'], function(angular, $) {
     .controller('LeaveBalanceController',
     ['$scope','hrsService',
     function($scope, hrsService){
-       var data = hrsService.getLeaveBalances();
+        $scope.greeting = "Hola!";
+        $scope.balances = [];
+        hrsService.getLeaveBalances().then(
+            function(balances) {
+                if (balances) {
+                    $scope.balances = balances.balances;
+                }
+                return balances;
+            }
+        )
     }])
 });
+/*--
+'use strict';
+
+define(['angular', 'jquery'], function (angular, $) {
+    return angular.module('view-home.controllers ', [])
+        .controller('LeaveBalanceController', [
+            '$scope', 'hrsService',
+            function ($scope, hrsService) {
+
+                var init = function () {
+                    $scope.greeting="Bonjour";
+                    $scope.loading = true;
+                    hrsService.getLeaveBalances().then(
+                        function (result) {
+                            if (!result) {
+                                $scope.error = true;
+                                $scope.leaveBalances = "{}";
+                            } else {
+                                $scope.error = false;
+                                $scope.balances = result.data;
+                            };
+                        });
+                };
+
+                init();
+            }])
+});
+*/
